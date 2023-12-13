10571394ac63fb44f0996f929c20b7dad699e571
Sidney Andrews <sidandrews@microsoft.com>
2023-12-13 14:39:05 -0500

    Fix bug with hard-coded broker address

--- src/web/src/main/java/com/microsoft/learn/azure/cosmosdb/nosql/quickstart/WebSocketConfig.java
+++ src/web/src/main/java/com/microsoft/learn/azure/cosmosdb/nosql/quickstart/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket");
+        registry.addEndpoint("/cosmos-db-nosql").withSockJS();
     }
 }
